08bf131c293eaaca6c807eb94fd1f71da67a6896
Azhar Prabudi <azharprabui@gmail.com>
2019-08-07 10:19:48 +0700

    create server

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import { buildTypeDefsAndResolvers } from "type-graphql";
+import { ApolloServer, makeExecutableSchema } from "apollo-server";
+import RootResolvers from "./resolvers";
+import container from "./utils/di";
+import PostResolver from "./resolvers/post";
+
+const main: () => Promise<ApolloServer> = async (): Promise<ApolloServer> => {
+  const { typeDefs, resolvers } = await buildTypeDefsAndResolvers({
+    container,
+    resolvers: [RootResolvers, PostResolver]
+  });
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  return new ApolloServer({
+    schema,
+    cors: true,
+    tracing: true,
+    debug: process.env.GQL_FAZZCRD_MODE == "production" ? false : true,
+    playground: process.env.GQL_FAZZCRD_MODE == "production" ? false : true
+  });
+};
+
+main()
+  .then((server: ApolloServer) => {
+    const port = process.env.GQL_FAZZCRD_PORT || 6060;
+    server.listen(port, () => {
+      console.log(`Your server already running at port ${port}`);
+    });
+  })
+  .catch(console.error);
